da05ec8f8c64f6a1490a101bbc6a7315176b107a
Muez Gidey <muezgidey8@gmail.com>
2025-08-06 15:57:53 +0300

    Add answers.sql for database assignment

---
+++ answers.sql
@@ -0,0 +1,18 @@
+-- ================================================
+-- Assignment: Introduction to Databases
+-- Student: Muez Gidey
+-- File: answers.sql
+-- Description: SQL queries to create and delete databases
+-- ================================================
+
+-- 🔹 Question 1: Write an SQL query to create a new database called salesDB
+
+-- This command creates a new database named 'salesDB'.
+CREATE DATABASE salesDB;
+
+
+-- 🔹 Question 2: Write an SQL query to drop (delete) the database called demo
+
+-- This command deletes the database named 'demo' if it exists.
+-- Be cautious: This operation is irreversible and will remove all data.
+DROP DATABASE demo;
